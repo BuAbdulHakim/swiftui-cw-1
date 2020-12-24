669faef999784300c5a90fb03343f66824a5259d
Nasser Alsaqabi <alsokor4@gmail.com>
2020-12-24 21:07:15 +0300

    Created cw-1 Project and the two files

---
+++ cw-1/cw-1/CW_1_2.swift
@@ -0,0 +1,20 @@
+//
+//  CW_1_2.swift
+//  cw-1
+//
+//  Created by NasserAlsaqabi on 24/12/2020.
+//
+
+import SwiftUI
+
+struct CW_1_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CW_1_2_Previews: PreviewProvider {
+    static var previews: some View {
+        CW_1_2()
+    }
+}
